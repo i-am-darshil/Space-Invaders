40346dac09551092f6a3dd509d18ac10f9344a85
Darshil Shah <darshilx@amazon.com>
2023-02-19 18:41:05 +0530

    06 - Player Movements

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 let playerImageScale = 0.15
+let playerSpeed = 3
 
 class Player {
   constructor() {
@@ -28,21 +29,112 @@ class Player {
 
 
   draw() {
-    // c.fillStyle = "red"
-    // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-
     c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
   }
 }
 
 let player = new Player()
+let keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  space: {
+    pressed: false
+  }
+}
 
 
 function animate() {
   window.requestAnimationFrame(animate)
   c.fillStyle = "black"
   c.fillRect(0, 0, canvas.width, canvas.height)
-  player.draw()
+  player.update()
+
+  player.velocity.x = 0
+  player.velocity.y = 0
+
+  if (keys.a.pressed) {
+    player.velocity.x = -playerSpeed
+  } else if (keys.d.pressed) {
+    player.velocity.x = playerSpeed
+  } else if (keys.w.pressed) {
+    player.velocity.y = -playerSpeed
+  } else if (keys.s.pressed) {
+    player.velocity.y = playerSpeed
+  }
 }
 
 animate()
+
+window.addEventListener('keydown', function(event) {
+  console.log("keydown : ", event)
+  switch (event.key) {
+    case "a":
+      console.log("left")
+      // player.velocity.x = -playerSpeed
+      keys.a.pressed = true
+      break;
+    case "d":
+      console.log("right")
+      // player.velocity.x = playerSpeed
+      keys.d.pressed = true
+      break;
+    case "w":
+      console.log("up")
+      // player.velocity.y = -playerSpeed
+      keys.w.pressed = true
+      break;
+    case "s":
+      console.log("down")
+      // player.velocity.x = playerSpeed
+      keys.s.pressed = true
+      break;
+    case " ":
+      console.log("space")
+      keys.space.pressed = true
+  }
+})
+
+window.addEventListener('keyup', function(event) {
+  console.log("keyup : ", event)
+  switch (event.key) {
+    case "a":
+      console.log("left")
+      // player.velocity.x = -playerSpeed
+      keys.a.pressed = false
+      break;
+    case "d":
+      console.log("right")
+      // player.velocity.x = playerSpeed
+      keys.d.pressed = false
+      break;
+    case "w":
+      console.log("up")
+      // player.velocity.y = -playerSpeed
+      keys.w.pressed = false
+      break;
+    case "s":
+      console.log("down")
+      // player.velocity.x = playerSpeed
+      keys.s.pressed = false
+      break;
+    case " ":
+      console.log("space")
+      keys.space.pressed = false
+  }
+})
