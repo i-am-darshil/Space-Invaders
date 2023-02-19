6df0c314c3a55c68f08e97d8d5de75b1d1d2ae22
Darshil Shah <darshilx@amazon.com>
2023-02-19 14:47:48 +0530

    03 - Load Assets

--- index.js
+++ index.js
@@ -16,16 +16,20 @@ class Player {
       x : 0,
       y : 0
     }
-    this.image = ""
+    this.image = new Image()
+    this.image.src = "./assets/spaceship.png"
     this.width = 100
     this.height = 100
   }
 
+
   draw() {
-    c.fillStyle = "red"
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    // c.fillStyle = "red"
+    // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    c.drawImage(this.image, this.position.x, this.position.y)
   }
 }
 
 let player = new Player()
+// While drawing images of KB's in size, it might take some time to load the image. Hence, just drawing once at the start might give us a white screen
 player.draw()
